27163d6b72f7cd0662d6540d02dd7add50f41e4d
luiss_benvenuto <luiss.benvenuto@gmail.com>
2026-03-15 22:55:31 -0600

    Allow rendering admin pages without redirecting by default

--- src/app/(pages)/admin/layout.tsx
+++ src/app/(pages)/admin/layout.tsx
@@ -1,14 +1,6 @@
-import { Role } from "@prisma/client";
-import { redirect } from "next/navigation";
-import { getServerAuthSession } from "rbrgs/server/auth";
-
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  const session = await getServerAuthSession();
-
-  if (session?.user.role !== Role.ADMIN) {
-    redirect("/");
-  }
+  // Allow rendering admin pages without redirecting by default
   return children;
 }
